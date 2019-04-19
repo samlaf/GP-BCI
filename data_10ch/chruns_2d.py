58457f90abe1a0e6e1a8bf6c56cbfb5c2cd6031b
Samuel Laferriere <samlaf92@gmail.com>
2019-04-19 14:01:06 -0400

    was missing --dt in chruns_2d

--- data_10ch/chruns_2d.py
+++ data_10ch/chruns_2d.py
@@ -5,6 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--uid', type=str, default=0, help='uid for job number (alphanumerical)')
 parser.add_argument('--emg', type=int, default=2, choices=range(7), help='emg. between 0-6')
+parser.add_argument('--dt', type=int, default=0, choices=(0,10,20,40,60,80,100), help='dt. one of (0,10,20,40,60,80,100)')
 
 if __name__ == "__main__":
     args = parser.parse_args()
