587db6f1fc8f958e12e239777fd6c24fac385166
Samuel Laferriere <samlaf92@gmail.com>
2019-04-19 13:55:15 -0400

    made uid a str

--- data_10ch/chruns_2d.py
+++ data_10ch/chruns_2d.py
@@ -3,7 +3,7 @@ from gp_full_2d import run_ch_stats_exps
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--uid', type=int, default=0, help='uid for job number')
+parser.add_argument('--uid', type=str, default=0, help='uid for job number (alphanumerical)')
 parser.add_argument('--emg', type=int, default=2, choices=range(7), help='emg. between 0-6')
 
 if __name__ == "__main__":
