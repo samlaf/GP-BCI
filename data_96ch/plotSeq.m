f4ca81a62c234f16d9a211e484e9124710dd5578
Samuel Laferriere <samlaf92@gmail.com>
2019-05-16 14:31:08 -0400

    Upgraded data_96ch experiments with most recent results

--- data_96ch/plotSeq.m
+++ data_96ch/plotSeq.m
@@ -1,4 +1,4 @@
-function plotSeq(means, P_test, hypoSEard, kappas)
+function plotSeq(means, P_test, hypo, kappas)
 %PLOTSEQ 此处显示有关此函数的摘要
 %   此处显示详细说明
 
@@ -17,42 +17,57 @@ queries = P_test(:,1);
 %ax = axes('Parent',f,'position',[0.13 0.39  0.77 0.54]);
 ax1 = subplot(2,2,1);
 h = imagesc(ax1,means(xy2ch));
+colorbar
 title('real mean')
-hold on;
-lastplot1 = 0;
-lastplot2 = 0;
 
-c = uicontrol('Style','slider','Position',[81,54,419,23],...
+c = uicontrol('Style','slider','Position',[81,10,419,10],...
               'value',1, 'min',1, 'max',96, 'SliderStep',[1/95 1/10]);
-not_first_event_flag=false;
 c.Callback = @selection;
 
     function selection(src,event)
         % We first plot the point queried
         idx = c.Value;
         [y_,x_] = find(xy2ch == queries(idx));
-        if not_first_event_flag
-            delete(lastplot1);
-        end
-        lastplot1 = plot(ax1, x_, y_, 'r+');
+        [y_next, x_next] = find(xy2ch == queries(idx+1));
         % Then plot the gp predictions
         x = ch2xy(P_test(1:idx,1),:);
         y = P_test(1:idx,2);
-        [ymu ys2 fmu fs2] = gp(hypoSEard, @infGaussLik, [], @covSEard, @likGauss, x, y, ch2xy);
+        [ymu ys2 fmu fs2] = gp(hypo, @infGaussLik, @meanConst, @covSEard, @likGauss, x, y, ch2xy);
         kappa = kappas(idx);
         acqmap = ymu + kappa* sqrt(ys2);
         [acqmax, acqargmax] = max(acqmap);
         
+        subplot(2,2,1);
+        h = imagesc(ax1,means(xy2ch));
+        colorbar
+        title('real mean')
+        hold on;
+        plot(x_, y_, 'r+');
+        plot(x_next, y_next, 'kx');
+        
+        subplot(2,2,2);
+        imagesc(acqmap(xy2ch));
+        colorbar
+        title('acquisition map');
+        hold on;
+        plot(x_, y_, 'r+');
+        plot(x_next, y_next, 'kx');
+        
         subplot(2,2,3);
         imagesc(ymu(xy2ch));
+        colorbar
         title('gp mean');
+        hold on;
+        plot(x_, y_, 'r+');
+        plot(x_next, y_next, 'kx');
+        
         subplot(2,2,4);
         imagesc(sqrt(ys2(xy2ch)));
+        colorbar
         title('gp std');
-        ax2 = subplot(2,2,2);
-        imagesc(acqmap(xy2ch));
-        title('acquisition map');
-        not_first_event_flag=true;
+        hold on;
+        plot(x_, y_, 'r+');
+        plot(x_next, y_next, 'kx');
     end
 
 end
